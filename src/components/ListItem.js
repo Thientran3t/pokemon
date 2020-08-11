08ef97b0ca61661a0216e29ca37b775e64dbf6c7
Thien Tran <ngocthien.tran@amaris.com>
2020-08-11 13:51:26 +0700

    render weakness

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,6 +3,7 @@ import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
+import '../styles/Item.css';
 
 const ListItem = (props) => {
     const classes = useStyles();
@@ -15,8 +16,19 @@ const ListItem = (props) => {
                         <Grid key={item.id} item>
                             <Paper className={classes.paper} >
                                 <Avatar variant='square' className={classes.square} src={item.ThumbnailImage}></Avatar>
-                                <div className={classes.textLeftNumber}>#{item.number}</div>
-                                <div className={classes.textLeftName}>{item.name}</div>
+                                <div className={classes.containerTitle}>
+                                    <div className={classes.textLeftNumber}>#{item.number}</div>
+                                    <div className={classes.textLeftName}>{item.name}</div>
+                                    <Grid container spacing={2}>
+                                        {item.weakness.map((weakness, i) => (
+                                            <Grid key={i} item xs={6}>
+                                                <div className={weakness}>
+                                                    {weakness}
+                                                </div>
+                                            </Grid>
+                                        ))}
+                                    </Grid>
+                                </div>
                             </Paper>
                         </Grid>
                     ))}
@@ -42,13 +54,27 @@ const useStyles = makeStyles((theme) => ({
         width: 200,
     },
     textLeftNumber: {
-        textAlign: 'left'
+        textAlign: 'left',
+        fontWeight: 'bold',
+        color: '#939393'
     },
     textLeftName: {
         textAlign: 'left',
         fontSize: 20,
-        marginTop: 10
-    }
+        marginTop: 10,
+        fontWeight: 'bold',
+    },
+    containerTitle: {
+        paddingLeft: 10
+    },
+    containerWeakness: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: 200
+    },
+    weakness: {
+        height: 20,
+    },
   }));
 
 export default ListItem;
